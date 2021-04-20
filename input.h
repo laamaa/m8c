a469e5d8c119113abf88f65bbb6853bd17a4585e
Jonne Kokkonen <jonne.kokkonen@gmail.com>
2021-04-20 17:11:53 +0300

    initial keyjazz support, reorganize input.c/.h

--- input.h
+++ input.h
@@ -1,10 +1,25 @@
 #ifndef INPUT_H_
 #define INPUT_H_
 
-#include <SDL2/SDL_joystick.h>
+#include <stdint.h>
+
+typedef enum input_type_t {
+  normal,
+  keyjazz,
+  special
+} input_type_t;
+
+typedef enum special_messages_t {
+  msg_quit
+} special_messages_t;
+
+typedef struct input_msg_s {
+  input_type_t type;
+  uint8_t value;
+} input_msg_s;
 
 int initialize_input();
 void close_input();
-uint8_t process_input();
+input_msg_s get_input_msg();
 
 #endif
