e56ddd40a182f80b21d88a5536d8fe68d6aaaf48
Andrew Hillel <subvertnormality@users.noreply.github.com>
2022-02-09 18:19:09 +0000

    Update main.c

--- main.c
+++ main.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]) {
 
   client = enet_host_create (NULL /* create a client host */,
               1 /* only allow 1 outgoing connection */,
-              2 /* allow up 2 channels to be used, 0 and 1 */,
+              3 /* allow up 2 channels to be used, 0 and 1 */,
               0 /* assume any amount of incoming bandwidth */,
               0 /* assume any amount of outgoing bandwidth */);
 
