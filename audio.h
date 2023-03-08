41c865e68c8d9e24eb67775b72f422587849eabc
laamaa <jonne.kokkonen@gmail.com>
2023-03-08 22:23:16 +0200

    clean audio.h

--- audio.h
+++ audio.h
@@ -1,12 +1,12 @@
+// Copyright 2021 Jonne Kokkonen
+// Released under the MIT licence, https://opensource.org/licenses/MIT
+
 #ifndef AUDIO_H
 #define AUDIO_H
 
 #include <stdint.h>
 
-//Recording/playback callbacks
-void audio_cb_in( void* userdata, uint8_t* stream, int len );
-void audio_cb_out( void* userdata, uint8_t* stream, int len );
-
+void audio_cb_in(void *userdata, uint8_t *stream, int len);
 int audio_init(int audio_buffer_size);
 void audio_destroy();
 
